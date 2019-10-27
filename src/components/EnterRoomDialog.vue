c838294dcecc14423d638ad5ecd81b353f6f4580
Allquantor <nilssmol@gmail.com>
2019-10-27 11:22:58 +0100

    Create and Enter Room Dialogs

---
+++ src/components/EnterRoomDialog.vue
@@ -0,0 +1,39 @@
+<template>
+  <form action>
+    <div class="modal-card" style="width: auto">
+      <header class="modal-card-head">
+        <p class="modal-card-title">Raum betreten</p>
+      </header>
+      <section class="modal-card-body">
+        <b-field label="Raumname">
+          <b-input v-model="roomName" value placeholder="Raumname" required></b-input>
+        </b-field>
+
+        <b-field label="Password">
+          <b-input v-model="roomPassword" placeholder="Passwort"></b-input>
+        </b-field>
+      </section>
+      <footer class="modal-card-foot">
+        <button class="button" type="button" @click="$parent.close()">Schliessen</button>
+        <button class="button is-primary" @click="enterRoom">Betreten</button>
+      </footer>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      roomName: "",
+      roomPassword: ""
+    };
+  },
+  methods: {
+    enterRoom() {
+      console.log(this.roomName);
+      console.log(this.roomPassword);
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,48 @@
   <div class="container" style="height:100%">
     <div class="columns is-vcentered is-centered is-multiline is-mobile" style="height:100%">
       <div class="column is-narrow">
-        <b-button type="is-primary">Raum erstellen</b-button>
+        <b-button type="is-primary" @click="isCreateRoomDialog = true">Raum erstellen</b-button>
       </div>
       <div class="column is-narrow">
-        <b-button type="is-primary">Raum beitreten</b-button>
+        <b-button type="is-primary" @click="isEnterRoomDialog = true">Raum beitreten</b-button>
       </div>
     </div>
+
+    <b-modal
+      :active.sync="isCreateRoomDialog"
+      has-modal-card
+      trap-focus
+      aria-role="dialog"
+      aria-modal
+    >
+      <CreateRoomDialog />
+    </b-modal>
+
+    <b-modal
+      :active.sync="isEnterRoomDialog"
+      has-modal-card
+      trap-focus
+      aria-role="dialog"
+      aria-modal
+    >
+      <EnterRoomDialog />
+    </b-modal>
   </div>
 </template>
 
 <script>
+import CreateRoomDialog from "../components/CreateRoomDialog";
+import EnterRoomDialog from "../components/EnterRoomDialog";
+
 export default {
   name: "home",
+  components: {
+    CreateRoomDialog,
+    EnterRoomDialog
+  },
+  data: () => {
+    return { isCreateRoomDialog: false, isEnterRoomDialog: false };
+  },
   methods: {}
 };
 </script>
